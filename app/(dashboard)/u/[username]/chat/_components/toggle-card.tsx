906ab3de5779c7d37f992a0fc78fb063722c6912
BilawalArif <bilawalarif003@gmail.com>
2024-02-26 09:22:24 +0500

    stream server action and chat settings

--- app/(dashboard)/u/[username]/chat/_components/toggle-card.tsx
+++ app/(dashboard)/u/[username]/chat/_components/toggle-card.tsx
@@ -4,6 +4,7 @@ import { toast } from "sonner";
 import { useTransition } from "react";
 
 import { Switch } from "@/components/ui/switch";
+import { updateStream } from "@/actions/stream";
 import { Skeleton } from "@/components/ui/skeleton";
 
 type FieldTypes = "isChatEnabled" | "isChatDelayed" | "isChatFollowersOnly";
@@ -21,12 +22,24 @@ export const ToggleCard = ({
 }: ToggleCardProps) => {
   const [isPending, startTransition] = useTransition();
 
+  const onChange = () => {
+    startTransition(() => {
+      updateStream({ [field]: !value })
+        .then(() => toast.success("Chat settings updated!"))
+        .catch(() => toast.error("Something went wrong"));
+    });
+  };
+
   return (
     <div className="rounded-xl bg-muted p-6">
       <div className="flex items-center justify-between">
         <p className="font-semibold shrink-0">{label}</p>
         <div className="space-y-2">
-          <Switch disabled={isPending} checked={value}>
+          <Switch
+            disabled={isPending}
+            onCheckedChange={onChange}
+            checked={value}
+          >
             {value ? "On" : "Off"}
           </Switch>
         </div>
