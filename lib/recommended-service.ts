524c450bba32be268d6457ba653a6b185c7cff17
BilawalArif <bilawalarif003@gmail.com>
2024-03-01 08:38:59 +0500

    fix data passed to isLive

--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -43,7 +43,11 @@ export const getRecommendedUsers = async () => {
         ],
       },
       include: {
-        stream: true,
+        stream: {
+          select: {
+            isLive: true,
+          },
+        },
       },
       orderBy: {
         createdAt: "desc",
@@ -52,7 +56,11 @@ export const getRecommendedUsers = async () => {
   } else {
     users = await db.user.findMany({
       include: {
-        stream: true,
+        stream: {
+          select: {
+            isLive: true,
+          },
+        },
       },
       orderBy: {
         createdAt: "desc",
