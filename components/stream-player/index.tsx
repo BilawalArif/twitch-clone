c9e07d4871aa79db86f095303ad01f6c84c6ef10
Bilawal Arif <bilawalarif003@gmail.com>
2024-10-26 23:25:34 +0500

    Add community component

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -3,11 +3,11 @@
 import { useViewerToken } from "@/hooks/user-viewer-tokes";
 import { Stream, User } from "@prisma/client";
 import { LiveKitRoom } from "@livekit/components-react";
-import { Video } from "./video";
+import { Video, VideoSkeleton } from "./video";
 import { useChatSidebar } from "@/store/use-chat-sidebar";
 import { cn } from "@/lib/utils";
-import { Chat } from "./Chat";
 import { ChatToggle } from "./chat-toggle";
+import { Chat, ChatSkeleton } from "./chat1";
 
 interface StreamPlayerProps {
   user: User & { stream: Stream | null };
@@ -19,7 +19,7 @@ const StreamPlayer = ({ user, stream, isFollowing }: StreamPlayerProps) => {
   const { collapsed } = useChatSidebar((state) => state);
 
   if (!token || !name || !identity) {
-    return <div>Cannot watch the stream</div>;
+    return <StreamPlayerSkeleton />;
   }
   return (
     <>
@@ -56,3 +56,17 @@ const StreamPlayer = ({ user, stream, isFollowing }: StreamPlayerProps) => {
 };
 
 export default StreamPlayer;
+
+export function StreamPlayerSkeleton() {
+  return (
+    <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
+      <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+        <VideoSkeleton />
+        {/* <HeaderSkeleton /> */}
+      </div>
+      <div className="col-span-1 bg-background">
+        <ChatSkeleton />
+      </div>
+    </div>
+  );
+}
